7ac22458de430d26216072892051a374bc467af5
bodoralex <bodaleaa@gmail.com>
2017-01-11 13:58:45 +0100

    justicedep runs

--- justicedepartment.py
+++ justicedepartment.py
@@ -10,31 +10,40 @@ class JusticeDepatrment:
     @classmethod
     def random_punishment(cls, persons):
         # lista, fekvő, tökönrúgás, lefejezés
-        print(persons)
 
         for person in persons[::-1]:
-            print(person)
             punishment_factor = random.randint(0, 100)
             print("{}'s punishment is: ".format(person.fullname), end="")
-            if True:  # punishment_factor > 95:
-                # lefejezés
+            if punishment_factor > 95:
                 cls.decapitation(person)
-                pass
-            elif punishment_factor > 80:
-                # tökönrúgás
-                pass
             elif punishment_factor > 50:
-                # lista
-                pass
+                cls.punish_presentation(person)
             else:
-                # fekvő
-                pass
+                cls.pushups(person)
 
     @classmethod
     def decapitation(cls, person):
         print("decapitation.")
         person.kill()
 
-codecool_msc = CodecoolClass.create_local()
-#kisclass = [Student("Laci", "Kovacs", 1992, "male", 11)]
-JusticeDepatrment.random_punishment(codecool_msc.students)
+    @classmethod
+    def punish_presentation(cls, person):
+        print("punishment presentation")
+        person.energy -= 25 + random.randint(-5, 10)
+        person.knowledge_level += 30 + random.randint(0, 20)
+
+    @classmethod
+    def pushups(cls, person):
+        print("pushups")
+        person.energy -= 15 + random.randint(0, 10)
+
+
+def main():
+
+    codecool_msc = CodecoolClass.create_local()
+    #kisclass = [Student("Laci", "Kovacs", 1992, "male", 11)]
+    JusticeDepatrment.random_punishment(codecool_msc.students)
+
+
+if __name__ == "main":
+    main()
--- story.py
+++ story.py
@@ -2,13 +2,13 @@ from codecool_class import CodecoolClass
 from mentor import Mentor
 from student import Student
 from morningrutine import MorningRoutine
+from justicedepartment import JusticeDepatrment
 
 codecool_msc = CodecoolClass.create_local()
 
 late_students = MorningRoutine.random_things(codecool_msc.students)
-
+JusticeDepatrment.random_punishment(late_students)
 """
-JusticeDepartment(late_student)
 Leraning.morning_dojo(codecool_msc)
 NaturalDiasaster(codecool_msc)
 late_students = MorningRoutine(codecool_msc)"""
