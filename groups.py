e55667d77add9e06611185902ed09b04893ec24e
bodoralex <bodaleaa@gmail.com>
2017-01-11 17:47:54 +0100

    pairs working

---
+++ groups.py
@@ -0,0 +1,65 @@
+class Pair:
+
+    pairs = []
+
+    def __init__(self, persons):
+        self.members = [persons[0], persons[1]]
+
+    @classmethod
+    def create_pairs(cls, persons):
+        pairs_to_instantiate = []
+        for student in persons.students:
+            pairs_to_instantiate.append(student)
+            if len(pairs_to_instantiate) == 2:
+                Pair.pairs.append(Pair(pairs_to_instantiate))
+                pairs_to_instantiate.clear()
+        if len(pairs_to_instantiate) == 1:
+            pairs_to_instantiate.append(persons.mentors[0])
+            Pair.pairs.append(Pair(pairs_to_instantiate))
+        return Pair.pairs
+
+
+"""class Group:
+
+    def __init__(self, persons):
+
+    @property
+    def overall_energy(self):
+        overall_energy = 0
+        for person in self.persons:
+            if hasattr(person, _energy):
+                overall_energy += person._energy
+        return overall_energy
+
+    @property
+    def avg_energy(self):
+        energy_list = []
+        for person in self.persons:
+            if hasattr(person, _energy):
+                energy_list.append(person.energy)
+        return sum(energy_list) / len(energy_list)
+
+    @property
+    def avg_skill(self):
+        skills_levels = []
+        pass
+
+
+class Pair(Group):
+
+    pairs = []
+
+    def __init__(self, persons):
+        super().__init__(persons)
+
+    def create_pairs(cls, persons):
+        temp_pairs = []
+        for i in range(len(persons.students)):
+            temp_pairs.append(persons.students[i])
+            if len(temp_pairs) == 2:
+                Pair.pairs.append(Pair(temp_pairs))
+                temp_pairs.clear()
+        if len(temp_pairs) == 1:
+            temp_pairs.append(persons.mentors[0])
+            Pair.pairs.append(Pair(temp_pairs))
+        return Pair.pairs"""
--- story.py
+++ story.py
@@ -3,12 +3,20 @@ from mentor import Mentor
 from student import Student
 from morningrutine import MorningRoutine
 from justicedepartment import JusticeDepatrment
+from groups import *
 
 codecool_msc = CodecoolClass.create_local()
 
+
 late_students = MorningRoutine.random_things(codecool_msc.students)
 JusticeDepatrment.random_punishment(late_students)
+print(codecool_msc.students)
+coder_pairs = Pair.create_pairs(codecool_msc)
+print(coder_pairs[0].members)
+print(coder_pairs[1].members)
 """
+Learn.pairprograming(coder_pairs)
 Leraning.morning_dojo(codecool_msc)
 NaturalDiasaster(codecool_msc)
-late_students = MorningRoutine(codecool_msc)"""
+late_students = MorningRoutine(codecool_msc)
+"""
