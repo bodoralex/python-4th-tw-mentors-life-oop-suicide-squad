cdfe94c309fe51630e494db33d1563a67d02b5ae
ujfalusizsolt1 <ujfalusi.zsolt@gmail.com>
2017-01-12 15:09:04 +0100

    start of colour table removes

--- morningrutine.py
+++ morningrutine.py
@@ -1,6 +1,20 @@
 from person import Person
 from student import Student
 import random
+import color
+from color import colors
+
+# színek printeléshez
+PURPLE = '\033[95m'
+CYAN = '\033[96m'
+DARKCYAN = '\033[36m'
+BLUE = '\033[94m'
+GREEN = '\033[92m'
+YELLOW = '\033[93m'
+RED = '\033[91m'
+BOLD = '\033[1m'
+UNDERLINE = '\033[4m'
+END = '\033[0m'
 
 
 class MorningRoutine:
@@ -34,7 +48,7 @@ class MorningRoutine:
 
                 elif random.randint(1, 10) == 10:
                     print(person.fullname,
-                          "had a great sleep and became a super saiyan.")
+                          "had a great sleep and became a " + colors['YELLOW'] + colors['BOLD'] + "SUPER SAIYAN." + colors['END'])
                     person.energy = 9001
                     person.knowledge_level += 30 + random.randint(0, 10)
                 else:
